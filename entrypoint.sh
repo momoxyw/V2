892af8081004c270634b656a67722d1354b20541
momoxyw <105403952+momoxyw@users.noreply.github.com>
2026-01-18 13:21:01 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ nginx
 nohup ./et-linux-amd64 -l ws://[::]:8880 token a1b2c3 > xtunnel.log 2>&1 &
 # 稍等 1 秒确保 8880 端口就绪，然后启动 CF 隧道
 sleep 1
-nohup cloudflared tunnel --url http://[::]:8880 > cf_xt.log 2>&1 &
+nohup cloudflared tunnel --url ws://[::]:8880 > cf_xt.log 2>&1 &
 
 # 5. 【后台运行】哪吒探针安装 (不阻塞主流程)
 if [ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ]; then
