2e6e7c3a2e9373763306a19a7a06ec4abd5c92cf
momoxyw <105403952+momoxyw@users.noreply.github.com>
2026-05-14 16:26:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,44 @@
 #!/usr/bin/env bash
 
-# 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-base64 -d config > config.json
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
-sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" config.json
-sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" /etc/nginx/nginx.conf
-sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
+# 1. 变量准备 (使用 export 确保全局可见)
+export UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+export VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
+export VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
+export V3_PATH=${V3_PATH:-'/vlesspacket'} # 补全你日志里的 XHTTP 路径
 
-# 伪装 v2ray 执行文件
+# 2. 生成随机执行文件名 (必须放在使用它之前!)
 RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
-mv v ${RELEASE_RANDOMNESS}
-cat config.json | base64 > config
-rm -f config.json
 
-# 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
-#[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && echo '0' | ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
-[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && echo '0' | ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY} --tls
-
-# 运行 nginx 和 v2ray
-nginx
+# 3. 处理配置文件
 base64 -d config > config.json
+# 替换 Xray 配置中的占位符
+sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#V3_PATH#${V3_PATH}#g" config.json
+
+# 4. 修改 Nginx 配置 (这是解决 404 的关键)
+# 确保你的 nginx.conf 里有对应的 V3_PATH 占位符
+sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#V3_PATH#${V3_PATH}#g" /etc/nginx/nginx.conf
+
+# 5. 特殊处理：UUID 路径管理页 (注入到 Nginx)
+# 这样你访问 你的域名/你的UUID 就能看到 info.html
+if [ -f /etc/nginx/nginx.conf ]; then
+    sed -i "/location \/ {/i \
+    location /${UUID} { \
+        root /usr/share/nginx/html; \
+        index info.html; \
+        try_files \$uri \$uri/ /info.html =404; \
+    }" /etc/nginx/nginx.conf
+fi
+
+# 6. 伪装执行文件
+mv v ./${RELEASE_RANDOMNESS}
+
+# 7. 哪吒探针 (保持原样)
+if [ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ]; then
+    wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh
+    ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY} --tls >/dev/null 2>&1 &
+fi
+
+# 8. 启动服务
+nginx
+echo "服务启动成功，执行文件名: ${RELEASE_RANDOMNESS}"
 ./${RELEASE_RANDOMNESS} -config=config.json
